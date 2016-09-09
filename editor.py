66b67fbf1ca5e1d9f448e0ec6bc33891f0e05b7b
etmoore <etmoore@gmail.com>
2016-09-08 22:09:47 -0600

    read quotes from moviequotes file

---
+++ editor.py
@@ -0,0 +1,11 @@
+# make a list of curse words
+# import the text,
+def read_text():
+    quotes = open('./movie_quotes.txt').read()
+    print quotes
+
+# separate it by word
+# compare each word to the list of curse words
+# if there's a match, alert the user to the profanity
+
+read_text()
